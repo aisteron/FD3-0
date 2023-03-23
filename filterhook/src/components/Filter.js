f715a1f453a68186129c49f689fa3b397e65aa7f
aisteron <timotheus@list.ru>
2023-03-23 23:01:39 +0300

    FilterProvider

--- filterhook/src/components/Filter.js
+++ filterhook/src/components/Filter.js
@@ -1,17 +1,18 @@
-import {useReducer} from 'react'
+
 import Controls from "./Controls";
 import List from "./List";
-import {ContextApp, initialState, testReducer} from "../reducer";
+
 
 export default function Filter(){
   
-  const [state, dispatch] = useReducer(testReducer, initialState);
+  //const [state, dispatch] = useReducer(testReducer, initialState);
 
   
   return(
-    <ContextApp.Provider value={{dispatch, state}}>
-      <Controls />
-      <List/>
-    </ContextApp.Provider> 
+      <>
+        <Controls />
+        <List/>
+      </>
+    
   )
 }
--- filterhook/src/index.js
+++ filterhook/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Filter from './components/Filter';
+import { FilterProvider } from './reducer';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Filter/>
+    <FilterProvider>
+      <Filter/>
+    </FilterProvider>
   </React.StrictMode>
 );
 
